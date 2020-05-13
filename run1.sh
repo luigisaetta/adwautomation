8ddde32f82ffb2c1258541ec2cfcb458e20f27c3
Luigi Saetta <lsaetta@MBP-di-Luigi.local>
2020-05-13 09:49:50 +0200

    script per run simulazione carico

---
+++ run1.sh
@@ -0,0 +1,14 @@
+./charbench -c ../configs/SOE_Server_Side_V2.xml \
+            -cf ~/Wallet_ATP1.zip \
+            -cs atp1_tp \
+            -u soe \
+            -p Sogei1234567 \
+            -v users,tps \
+            -intermin 0 \
+            -intermax 0 \
+            -min 0 \
+            -max 0 \
+            -uc 128 \
+            -di SQ,WQ,WA \
+            -rt 0:10.00
+
